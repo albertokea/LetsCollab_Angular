db9bea2af5d47384131588876c07ebc778f274a9
albertokea <atorrescota5@gmail.com>
2021-03-08 17:06:08 +0100

    Componente cover añadido. Boostrap importado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoverComponent } from './components/cover/cover.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: CoverComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
